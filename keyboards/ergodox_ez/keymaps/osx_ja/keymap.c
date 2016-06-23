88aca99826ab8b52f471f886a78fb8be0b7bd6ce
zaru@sakuraba <zarutofu@gmail.com>
2016-06-23 23:25:25 +0900

    move keymap

--- keyboards/ergodox_ez/keymaps/osx_ja/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_ja/keymap.c
@@ -36,19 +36,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,
-        MO(MDIA),           KC_GRV,       LALT(KC_LSFT),  ALT_T(KC_APP),  KC_LGUI,
-                                               KC_ESC,KC_RGHT,
+        MO(MDIA),       KC_GRV,       LALT(KC_LSFT),  ALT_T(KC_APP),  KC_LGUI,
+                                                      KC_LGUI,KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_LEFT,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_RSFT,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,       KC_EQL,
+             KC_RSFT,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_EQL,
                                   KC_RGUI,KC_LBRC,KC_RBRC,KC_MINS,          KC_RSFT,
-             KC_PGUP,KC_PGDN,
+             KC_PGUP,     KC_ESC,
              KC_QUOT,
-             KC_RIGHT,KC_TAB, KC_ENT
+             KC_RIGHT,    KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
