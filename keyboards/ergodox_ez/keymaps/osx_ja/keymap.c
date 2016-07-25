cf990690a76d261255621df1cedc130befea7613
zaru@sakuraba <zarutofu@gmail.com>
2016-07-26 04:28:33 +0900

    fixed

--- keyboards/ergodox_ez/keymaps/osx_ja/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_ja/keymap.c
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|LShift|           |RShift|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |   = +  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L2  |  `~  |AltShf| App  | LGui |                                       | RGui |  [{  |  ]}  |  -_  | RShift |
+ *   |  L2  |  ALT | LGui | ALT  | LGui |                                       | RGui |  [{  |  ]}  |  -_  | RShift |
  *   `----------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Esc  | Right|       | PgUp | PgDn |
+ *                                        | Esc  | Right|       | PgUp |  Esc |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | '"   |        |      |
  *                                 |Space |Backsp|------|       |------|  Tab   |Enter |
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,
-        MO(MDIA),       KC_GRV,       LALT(KC_LSFT),  ALT_T(KC_APP),  KC_LGUI,
+        MO(MDIA),       KC_LALT,      KC_LGUI,KC_LALT,KC_LGUI,
                                                       KC_LGUI,KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_LEFT,
