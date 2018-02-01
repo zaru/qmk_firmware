afb8103f9fedc4195e41d37519a2e59deed4d5b8
zaru@sakuraba <zarutofu@gmail.com>
2018-02-01 14:12:53 +0900

    Fixed debounce

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -96,7 +96,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * runs seeming to need it. This may change when configuration for
  * this is more directly exposed.
  */
-#define DEBOUNCE    15
+#define DEBOUNCE    20
 
 #define PREVENT_STUCK_MODIFIERS
 
